a3fed175d535b732e3adfd211e40122b7374f352
Brian Rush <yankeh@gmail.com>
2017-05-12 16:59:38 -0700

    add front-end logic to push checked checkbox names to toppingArray property of pizzaOne object (equivalent prototype wasn't working)

--- js/scripts.js
+++ js/scripts.js
@@ -42,18 +42,18 @@ Pizza.prototype.setIndividToppingCost = function() {
 }
 
 //Prototype to push toppings to toppingsArray
-Pizza.prototype.pushToToppingsArray = function() {
-  var checkCheckBoxValue = document.getElementsByClassName('checkbox');
-  // var checkBoxArray = [];
-
-  for (i = 0; i < this.toppingsArray.legth(); i++) {
-    if (checkCheckBoxValue[i].checked === true) {
-      toppingsArray.push(checkCheckBoxValue[i].name);
-    }
-  }
-  return this.toppingsArray;
-  // this.toppingsArray.push(getElementsByClassName('checkbox'))
-}
+// Pizza.prototype.pushToToppingsArray = function() {
+//   var checkCheckBoxValue = document.getElementsByClassName('check');
+//   // var checkBoxArray = [];
+//
+//   for (i = 0; i < this.toppingsArray.length; i++) {
+//     if (checkCheckBoxValue[i].checked === true) {
+//       this.toppingsArray.push(checkCheckBoxValue[i].name);
+//     }
+//   }
+//   return this.toppingsArray;
+//   // this.toppingsArray.push(getElementsByClassName('checkbox'))
+// }
 
 //Prototype to set the number of toppings for a given pizza
 Pizza.prototype.setNumToppings = function() {
@@ -61,7 +61,7 @@ Pizza.prototype.setNumToppings = function() {
 }
 
 // Prototype to calculate the cost of a given pizza. Incorporates previous pizza prototypes inside itself.
-Pizza.prototype.calculatePizzaCost = function(baseCost) {
+Pizza.prototype.calculatePizzaCost = function() {
   this.setBaseCost();
   this.setIndividToppingCost();
   this.setNumToppings();
@@ -75,86 +75,88 @@ var pizzaOne = new Pizza();
 //USER INTERFACE LOGIC//
 $(document).ready(function() {
 
-  // Number of pizzas input
-  $('.btn-number').click(function(e) {
-    e.preventDefault();
-    var fieldName = $(this).attr('data-field');
-    var type      = $(this).attr('data-type');
-    var input = $("input[name='"+fieldName+"']");
-    var currentVal = parseInt(input.val());
-    if (!isNaN(currentVal)) {
-      if(type == 'minus') {
-        var minValue = parseInt(input.attr('min'));
-        if(!minValue) minValue = 1;
-        if(currentVal > minValue) {
-          input.val(currentVal - 1).change();
-        }
-        if(parseInt(input.val()) == minValue) {
-          $(this).attr('disabled', true);
-        }
-      } else if(type == 'plus') {
-        var maxValue = parseInt(input.attr('max'));
-        if(!maxValue) maxValue = 5;
-        if(currentVal < maxValue) {
-          input.val(currentVal + 1).change();
-        }
-        if(parseInt(input.val()) == maxValue) {
-          $(this).attr('disabled', true);
-        }
-      }
-    } else {
-      input.val(0);
-    }
-  });
-  // More number of pizzas input stuff
-  $('.input-number').focusin(function(){
-    $(this).data('oldValue', $(this).val());
-  });
-  // More number of pizzas input stuff
-  $('.input-number').change(function() {
-    var minValue =  parseInt($(this).attr('min'));
-    var maxValue =  parseInt($(this).attr('max'));
-    if(!minValue) minValue = 1;
-    if(!maxValue) maxValue = 5;
-    var valueCurrent = parseInt($(this).val());
-    var name = $(this).attr('name');
-    if(valueCurrent >= minValue) {
-      $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
-    } else {
-      alert("Trust us: you don't want to order zero pizzas!");
-      $(this).val($(this).data('oldValue'));
-    }
-    if(valueCurrent <= maxValue) {
-      $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
-    } else {
-      alert("Oh no! You must be hungry. Unfortunately, we're not able to process more than 5 pizzas online at a time. Call 555-555-5555 and we'd be happy to help fulfill your sizable pizza needs!");
-      $(this).val($(this).data('oldValue'));
-    }
-  });
-  // More number of pizzas input stuff
-  $(".input-number").keydown(function (e) {
-      // Allow: backspace, delete, tab, escape, enter and .
-      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-           // Allow: Ctrl+A
-          (e.keyCode == 65 && e.ctrlKey === true) ||
-           // Allow: home, end, left, right
-          (e.keyCode >= 35 && e.keyCode <= 39)) {
-               // let it happen, don't do anything
-               return;
-      }
-      // Ensure that it is a number and stop the keypress
-      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-          e.preventDefault();
-      }
-    });
-  });
+  // // Number of pizzas input
+  // $('.btn-number').click(function(e) {
+  //   e.preventDefault();
+  //   var fieldName = $(this).attr('data-field');
+  //   var type      = $(this).attr('data-type');
+  //   var input = $("input[name='"+fieldName+"']");
+  //   var currentVal = parseInt(input.val());
+  //   if (!isNaN(currentVal)) {
+  //     if(type == 'minus') {
+  //       var minValue = parseInt(input.attr('min'));
+  //       if(!minValue) minValue = 1;
+  //       if(currentVal > minValue) {
+  //         input.val(currentVal - 1).change();
+  //       }
+  //       if(parseInt(input.val()) == minValue) {
+  //         $(this).attr('disabled', true);
+  //       }
+  //     } else if(type == 'plus') {
+  //       var maxValue = parseInt(input.attr('max'));
+  //       if(!maxValue) maxValue = 5;
+  //       if(currentVal < maxValue) {
+  //         input.val(currentVal + 1).change();
+  //       }
+  //       if(parseInt(input.val()) == maxValue) {
+  //         $(this).attr('disabled', true);
+  //       }
+  //     }
+  //   } else {
+  //     input.val(0);
+  //   }
+  // });
+  // // More number of pizzas input stuff
+  // $('.input-number').focusin(function(){
+  //   $(this).data('oldValue', $(this).val());
+  // });
+  // // More number of pizzas input stuff
+  // $('.input-number').change(function() {
+  //   var minValue =  parseInt($(this).attr('min'));
+  //   var maxValue =  parseInt($(this).attr('max'));
+  //   if(!minValue) minValue = 1;
+  //   if(!maxValue) maxValue = 5;
+  //   var valueCurrent = parseInt($(this).val());
+  //   var name = $(this).attr('name');
+  //   if(valueCurrent >= minValue) {
+  //     $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+  //   } else {
+  //     alert("Trust us: you don't want to order zero pizzas!");
+  //     $(this).val($(this).data('oldValue'));
+  //   }
+  //   if(valueCurrent <= maxValue) {
+  //     $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+  //   } else {
+  //     alert("Oh no! You must be hungry. Unfortunately, we're not able to process more than 5 pizzas online at a time. Call 555-555-5555 and we'd be happy to help fulfill your sizable pizza needs!");
+  //     $(this).val($(this).data('oldValue'));
+  //   }
+  // });
+  // // More number of pizzas input stuff
+  // $(".input-number").keydown(function (e) {
+  //     // Allow: backspace, delete, tab, escape, enter and .
+  //     if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+  //          // Allow: Ctrl+A
+  //         (e.keyCode == 65 && e.ctrlKey === true) ||
+  //          // Allow: home, end, left, right
+  //         (e.keyCode >= 35 && e.keyCode <= 39)) {
+  //              // let it happen, don't do anything
+  //              return;
+  //     }
+  //     // Ensure that it is a number and stop the keypress
+  //     if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+  //         e.preventDefault();
+  //     }
+  //   });
+  // });
 
   // Submit Listener
-  $('#submit-order').submit(function(event) {
+  $('#pizza-one').submit(function(event) {
     event.preventDefault();
-
-    // new Order();
-    new Pizza();
-
+      console.log("hello");
+      // pizzaOne.pushToToppingsArray(document);
+      $(".checkbox input:checked").each(function(){
+        pizzaOne.toppingsArray.push($(this).attr('name'));
+      })
+      // pizzaOne.toppingsArray.push($("input[type='checkbox']").name());
   });
-// });
+});
