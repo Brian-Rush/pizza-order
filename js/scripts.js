e92ca8178b8dca68ffdc57422f1f467be42e54c0
Brian Rush <yankeh@gmail.com>
2017-05-12 09:55:56 -0700

    add initial scaffolding to scripts.js file. Document ready, click listener, submit handler.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//BUSINESS LOGIC//
+
+
+
+//USER INTERFACE LOGIC//
+$(document).ready(function() {
+
+  $('.btn-number').click(function(event) {
+    event.preventDefault();
+
+  });
+
+  $('#submit-order').submit(function(event) {
+    event.preventDefault();
+
+  });
+});
